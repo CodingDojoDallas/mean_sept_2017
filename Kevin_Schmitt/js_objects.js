0c5fb123a3a4604c7602e1921e0fa110c47d775c
KEVIN SCHMITT <kschmit1@aisd.net>
2017-09-06 09:11:08 -0500

    je objects

---
+++ Kevin_Schmitt/js_objects.js
@@ -0,0 +1,32 @@
+let students = [
+    {name: 'Remy', cohort: 'Jan'},
+    {name: 'Genevieve', cohort: 'March'},
+    {name: 'Chuck', cohort: 'Jan'},
+    {name: 'Osmund', cohort: 'June'},
+    {name: 'Nikki', cohort: 'June'},
+    {name: 'Boris', cohort: 'June'},
+];
+for(let kid in students){
+    console.log('Name: ' + students[kid].name + ', Cohort: ' + students[kid].cohort)
+}
+
+
+let users = {
+    employees: [
+        {'first_name':  'Miguel', 'last_name' : 'Jones'},
+        {'first_name' : 'Ernie', 'last_name' : 'Bertson'},
+        {'first_name' : 'Nora', 'last_name' : 'Lu'},
+        {'first_name' : 'Sally', 'last_name' : 'Barkyoumb'}
+    ],
+    managers: [
+       {'first_name' : 'Lillian', 'last_name' : 'Chambers'},
+       {'first_name' : 'Gordon', 'last_name' : 'Poe'}
+    ]
+ };
+//  console.log('EMPLOYEES')
+for(let user in users){
+    console.log(users[user])
+}
+
+// console.log('MANAGERS')
+
