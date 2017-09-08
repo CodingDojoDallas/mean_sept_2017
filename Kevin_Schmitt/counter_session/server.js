c1fac98cf0c6055de84d9fa1dcc5e5f51a932251
KEVIN SCHMITT <kschmit1@aisd.net>
2017-09-08 12:26:46 -0500

    counter complete (folder: counter_session)

---
+++ Kevin_Schmitt/counter_session/server.js
@@ -0,0 +1,57 @@
+var express = require('express');
+let bodyParser = require('body-parser');
+let session = require('express-session');
+var port = 8000;
+
+var app = express();
+
+// now set up the middleware (.set)
+
+// .set
+// console.log('dirname =' , __dirname + '/views');
+// views
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+// static
+// .use
+app.use(express.static(__dirname + '/static'))
+// POST request helper
+app.use(bodyParser.urlencoded({ extended: true }));
+// session
+app.use(session({
+    secret: 'somethingsecretdoesnotmatterwhatyouput',
+    resave: false,
+    saveUninitialized: true,
+}))
+
+
+
+
+// routes
+app.get('/', (request, response) => {
+    if(!request.session.count){
+        request.session.count = 1;
+    }
+    else{
+        request.session.count++;
+    }
+    response.render("index.ejs", {count: request.session.count});
+})
+// app.get('/nextPage', function(req, res) {
+//     res.render("nextpage.ejs");
+// })
+
+
+
+
+
+
+
+
+
+
+
+// always goes at the bottom
+app.listen(port, () => {
+    console.log(`...shhh...Listening on port ${port}...`);
+})
--- Kevin_Schmitt/first_express/server.js
+++ Kevin_Schmitt/first_express/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-// let bodyParser = require('body-parser');
+let bodyParser = require('body-parser');
 // let session = require('express-session');
 var port = 8000;
 
@@ -8,16 +8,33 @@ var app = express();
 // console.log('dirname =' , __dirname + '/views');
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-
+let users = [
+    { id: 1, name: 'Kevin'},
+    { id: 2, name: 'Bob'},
+    { id: 3, name: 'Tim'},
+    { id: 4, name: 'Karen'},
+]
 // .use
 app.use(express.static(__dirname + '/static'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // routes
 app.get('/', function(request, response) {
-    response.render("index.ejs");
+    
+    response.render("index.ejs", { 'users': users });
 })
 app.get('/nextPage', function(request, response) {
     response.render("nextpage.ejs");
+})
+app.post('/users', function(request, response){
+    console.log(request.body.name);
+    let newId = users[users.length -1].id + 1
+    let userObj = {
+        id: newId,
+        name: request.body.name
+    }
+    users.push(userObj);
+    response.redirect('/')
 })
 
 
