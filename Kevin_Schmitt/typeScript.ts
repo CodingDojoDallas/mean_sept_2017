6c1c041bf663b0000ae6822f73e425c164171bd4
KEVIN SCHMITT <kschmit1@aisd.net>
2017-09-15 12:30:56 -0500

    types and classes assignment updated

--- Kevin_Schmitt/typeScript.ts
+++ Kevin_Schmitt/typeScript.ts
@@ -3,6 +3,8 @@ let myString:string = "Hello Universe";
 var myArr:number[] = [1,2,3,4];
 // or
 var myArr:Array<number> = [1,2,3,4];
+
+// objec - doesn't matter what's in the Obj
 var myObj:object = { 
     name:'Bill',
 };
@@ -11,6 +13,7 @@ var anythingVariable:any = 25;
 var arrayOne:boolean[] = [true, false, true, true]; 
 // or
 var arrayOne:Array<boolean> = [true, false, true, true];
+// don't even have to specufy array if it's an Any
 var arrayTwo:any[] = [1, 'abc', true, 2];
 // or
 var arrayTwo:Array<any> = [1, 'abc', true, 2];
@@ -20,23 +23,50 @@ var myObj:object = {
 };
 
 
-// object constructor
-var MyNode = (function () {
-    function MyNode(val:number) {
-        this.val = 0;
-        this.val = val;
+// // object constructor
+// var MyNode = (function () {
+//     function MyNode(val:number) {
+//         this.val = 0;
+//         this.val = val;
+//     }
+//     MyNode.prototype.doSomething = function () {
+//         this._priv = 10;
+//     };
+//     return MyNode;
+// }());
+// var myNodeInstance = new MyNode(1);
+// console.log(myNodeInstance.val);
+// function myFunction() {
+//     console.log("Hello World");
+//     return;
+// }
+// function sendingErrors() {
+// 	throw new Error('Error message');
+// }
+
+
+class MyNode {
+    value:number;
+    left:object;
+    right:object;
+    _priv:number;
+    constructor(value:number){
+        this.value = value;
+        this.right = null;
+        this.left = null;
     }
-    MyNode.prototype.doSomething = function () {
+    dosomething(){
         this._priv = 10;
-    };
-    return MyNode;
-}());
-var myNodeInstance = new MyNode(1);
-console.log(myNodeInstance.val);
-function myFunction() {
-    console.log("Hello World");
-    return;
+    }
 }
-function sendingErrors() {
-	throw new Error('Error message');
+
+let myNodeInstance:MyNode = new MyNode(10);
+
+function myFunction():void {
+    console.log('hello world');
+    // no return since void
+}
+
+function sendingErrors():never {
+    throw new Error('Error message');
 }
