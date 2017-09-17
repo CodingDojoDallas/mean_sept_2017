7d6e6b3a82db2a4aedcd951e237ea367bef927e1
KEVIN SCHMITT <kschmit1@aisd.net>
2017-09-17 14:04:49 -0500

    timezone event 90% complete and switchboard 98% complete both found in the MyFirstAngularApp project

--- Kevin_Schmitt/myFirstAngularApp/src/app/app.component.ts
+++ Kevin_Schmitt/myFirstAngularApp/src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'FirstAngularApp';
+  name = 'Kevin';
+  birthday = '3/21/1988';
+  today = new Date();
+  dosomething() {
+    return 'This is your function';
+  };
+  myTimeZone = 0;
+  
+  mySwitch:boolean = false;
+
+  pstButton():void {
+    this.myTimeZone = 1
+  };
+  cstButton():void {
+    this.myTimeZone = 2
+  };
+  mstButton():void {
+    this.myTimeZone = 3
+  };
+  clearButton():void {
+    this.myTimeZone = 0
+  };
+  activateSwitch():void {
+    this.mySwitch = !this.mySwitch;
+  };
+  buttons = [
+    'on','on','on','on','on','on','on','on','on','on'
+  ]
+  status = true;
+  toggleSwitch(idx):void {
+    if(this.buttons[idx] = 'on'){
+    this.buttons[idx] = 'off'
+    } else if (this.buttons[idx]='off') {
+      this.buttons[idx] = 'on'
+    }
+  }
 }
