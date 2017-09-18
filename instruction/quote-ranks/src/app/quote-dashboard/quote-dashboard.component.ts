f5030e52ae20894c394dd8f479ce50a904d84f14
codyw9524 <codyw9524@gmail.com>
2017-09-18 15:15:10 -0500

    added quote-ranks demo

---
+++ instruction/quote-ranks/src/app/quote-dashboard/quote-dashboard.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote-dashboard',
+  templateUrl: './quote-dashboard.component.html',
+  styleUrls: ['./quote-dashboard.component.css']
+})
+export class QuoteDashboardComponent implements OnInit {
+  quotes: Quote[] = [];
+
+  createQuote(newQuote) {
+    // console.log('adding quote to database...');
+    this.quotes.push(newQuote);
+    // console.log(this.quotes);
+  }
+
+  destroyQuote(idx) {
+    this.quotes.splice(idx, 1);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
