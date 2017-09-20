bd990aed8f44503cd4cd0b92066f647eccb6a92d
codyw9524 <codyw9524@gmail.com>
2017-09-20 16:26:46 -0500

    added code from full-mean demo

--- instruction/full-mean/task-api/public/src/app/app.component.ts
+++ instruction/full-mean/task-api/public/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +7,4 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'app';
-
-  constructor(private _router: Router) { }
-
-  redirectToNews(): void {
-    this._router.navigateByUrl('news');
-  }
-
 }
