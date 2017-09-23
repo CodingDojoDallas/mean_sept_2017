32e66adf20c6ac70691a342aae00b0bb6cfd34dc
Your Name <Charlie@Charlies-MBP.home>
2017-09-23 18:05:42 -0500

    team manager-partial working pages and links

--- charlie_delaRosa/full_mean/team_manager/public/src/app/team/player-list/player-list.component.ts
+++ charlie_delaRosa/full_mean/team_manager/public/src/app/team/player-list/player-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Player } from '../../player';
+import { PlayerService } from '../../player.service';
 
 @Component({
   selector: 'app-player-list',
   templateUrl: './player-list.component.html',
   styleUrls: ['./player-list.component.css']
 })
-export class PlayerListComponent implements OnInit {
 
-  constructor() { }
+export class PlayerListComponent implements OnInit {
+  // @Input() players: Player[];
+  players: Player[];
+
+  constructor(private _playerService: PlayerService) { }
 
   ngOnInit() {
+    this.getPlayers();
+  }
+
+  getPlayers() {
+    this._playerService.index(players => this.players = players);
   }
 
 }
--- charlie_delaRosa/full_mean/team_manager/public/src/app/team/player-new/player-new.component.ts
+++ charlie_delaRosa/full_mean/team_manager/public/src/app/team/player-new/player-new.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Player } from '../../player';
+import { PlayerService } from '../../player.service';
 
 @Component({
   selector: 'app-player-new',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerNewComponent implements OnInit {
 
-  constructor() { }
+  newPlayer: Player = new Player();
+  errors: string[] = [];
+  @Output() createPlayerEvent = new EventEmitter();
+
+  constructor(private _playerService: PlayerService) { }
 
   ngOnInit() {
+  }
+
+  createPlayer() {
+    console.log(this.newPlayer);
+    this.errors = [];
+    this._playerService.create(
+      this.newPlayer,
+      player => {
+        if(player.errors) {
+          for(const key of Object.keys(player.errors)) {
+            const error = player.errors[key];
+            this.errors.push(error.message);
+          }
+        } else {
+          this.newPlayer = new Player();
+          this.createPlayerEvent.emit();
+        }
+      }
+    );
   }
 
 }
